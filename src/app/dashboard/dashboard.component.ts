ef145d1d00255f442a2aeadcdf2eebd92c63265c
AdrianValdes <adrianvaldeshdz@gmail.com>
2020-12-04 20:53:18 +0100

    Cleaned the design for the albums

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,30 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import {Album} from "../model/album";
-import {Artist} from "../model/artist"
-import {AlbumService} from "../services/album.service";
-import {ArtistService} from "../services/artist.service";
+import { Album } from '../model/album';
+import { Artist } from '../model/artist';
+import { AlbumService } from '../services/album.service';
+import { ArtistService } from '../services/artist.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-
-  albums: Album[] = []
-  artists: Artist[] = []
-  constructor(private albumService: AlbumService, private artistService: ArtistService  ) { }
+  albums: Album[] = [];
+  artists: Artist[] = [];
+  constructor(
+    private albumService: AlbumService,
+    private artistService: ArtistService
+  ) {}
 
   ngOnInit(): void {
-    this.getAlbums()
-    this.getArtists()
+    this.getAlbums();
+    this.getArtists();
   }
 
   getAlbums(): void {
-    this.albumService.getAlbums().subscribe(albums => this.albums = albums.slice(0,5))
+    this.albumService
+      .getAlbums()
+      .subscribe((albums) => (this.albums = albums.slice(0, 4)));
   }
   getArtists(): void {
-    this.artistService.getArtists().subscribe(artists => this.artists = artists.slice(0,5))
+    this.artistService
+      .getArtists()
+      .subscribe((artists) => (this.artists = artists.slice(0, 4)));
   }
-
 }
